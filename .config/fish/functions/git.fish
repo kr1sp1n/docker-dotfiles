cd3ff21605adb8698026238582daba825a1f5053
Krispin Schulz <krispin@posteo.de>
2016-10-11 21:11:09 +0000

    use

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,3 +1,3 @@
 function git
-  docker run -it --rm -v $SSH_HOME:/root/.ssh --volumes-from data --volumes-from dotfiles -w $PWD kr1sp1n/git $argv
+  docker run -it --rm --volumes-from dock --volumes-from data --volumes-from dotfiles -w $PWD kr1sp1n/git $argv
 end
