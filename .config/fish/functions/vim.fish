ae5ec8b8d9635f19929093f2032894c7cfa1df19
Krispin Schulz <krispin@posteo.de>
2016-10-11 16:56:08 +0000

    add npm as fish function

--- .config/fish/functions/vim.fish
+++ .config/fish/functions/vim.fish
@@ -1,3 +1,3 @@
 function vim
-  docker run -it --rm --volumes-from data --volumes-from dotfiles -w $PWD kr1sp1n/alpine-neovim $argv
+  docker run -it --rm --volumes-from data -w $PWD --volumes-from dotfiles --entrypoint nvim kr1sp1n/alpine-neovim $argv
 end
